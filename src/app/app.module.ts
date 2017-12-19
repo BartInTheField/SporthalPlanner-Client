950ec6e9e768c46682b1d5e6287864bb89a2b99e
Bart in 't Veld <bart.int.007@gmail.com>
2017-12-19 12:44:48 +0100

    Standar components added plus router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule, Http } from '@angular/http';
 
 import { AppComponent } from './app.component';
 
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { BarComponent } from './components/bar/bar.component';
 import { NavbarService } from './services/navbar.service';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './components/home/home.component';
+import { PlanComponent } from './components/plan/plan.component';
+import { CalendarComponent } from './components/calendar/calendar.component';
+import { WorkComponent } from './components/work/work.component';
+import { TimeComponent } from './components/time/time.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    BarComponent
+    BarComponent,
+    HomeComponent,
+    PlanComponent,
+    CalendarComponent,
+    WorkComponent,
+    TimeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    AppRoutingModule
   ],
   providers: [NavbarService],
   bootstrap: [AppComponent]
