0c3cdc9774021c1de9aecefb154cc6716d63ec4f
Bart in 't Veld <bart.int.007@gmail.com>
2017-12-19 11:53:52 +0100

    colors and navbar added

---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,18 @@
+import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss']
+})
+export class NavbarComponent implements OnInit {
+
+  public show: boolean;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
