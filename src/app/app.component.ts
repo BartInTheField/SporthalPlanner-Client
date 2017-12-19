b2c807daa8ea0aa8d076189181c22fe0fed0233e
Bart in 't Veld <bart.int.007@gmail.com>
2017-12-19 14:27:09 +0100

    start on plan

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { NavbarService } from './services/navbar.service';
+import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  navBarIsShowing: boolean;
+
+  constructor(private navbarService: NavbarService) {
+
+  }
+
+  ngOnInit() {
+    this.navbarService.navbarIsOut.subscribe(next => {
+      this.navBarIsShowing = next;
+    });
+  }
+
 }
--- src/app/components/plan/plan.component.ts
+++ src/app/components/plan/plan.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlanComponent implements OnInit {
 
+  public bookings = [{}, {}, {}];
+
   constructor() { }
 
   ngOnInit() {
