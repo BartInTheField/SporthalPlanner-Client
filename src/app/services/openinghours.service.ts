bb8fa61f7173f6f745d69482b582040e69242bbe
Niels van Dam <nielsvdam@hotmail.com>
2017-12-20 14:13:58 +0100

    Openinghours service now has a get method to request all openinghours

--- src/app/services/openinghours.service.ts
+++ src/app/services/openinghours.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers, URLSearchParams } from '@angular/http';
 import { environment } from '../../environments/environment';
+import { Subject } from 'rxjs/Subject';
+import { OpeningHours } from '../models/openingHours.model';
 
 @Injectable()
 export class OpeningHoursService {
+    openingHoursChanged = new Subject<OpeningHours[]>();
     private headers = new Headers({ 'Content-Type' : 'application/json'});
     private serverUrl = environment.serverUrl + '/openinghours';
+
+    private openingHours: OpeningHours[] = [];
+
+    constructor(private http: Http) {}
+
+    /**
+     * getOpeningHoursAsync. Returns a promise
+     */
+    public getOpeningHoursAsync(): Promise<OpeningHours[]> {
+        return this.http.get(this.serverUrl, {headers : this.headers})
+        .toPromise()
+        .then((response) => {
+            this.openingHours = response.json() as OpeningHours[];
+            this.openingHoursChanged.next(this.openingHours);
+        })
+        .catch((error) => {
+            return this.handleError(error);
+        });
+    }
+
+    private handleError(error: any): Promise<any> {
+        console.log('handleError');
+        return Promise.reject(error.message || error);
+    }
 }
