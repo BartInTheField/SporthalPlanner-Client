68c69d81e0c03fb5e48a6f66fe9859e60a9e33e1
Bart in 't Veld <bart.int.007@gmail.com>
2017-12-20 14:26:46 +0100

    fake data

--- src/models/booking.model.ts
+++ src/models/booking.model.ts
@@ -2,7 +2,7 @@ import { SportsHallField } from "./sportsHallField.model";
 import { User } from "./user.model";
 
 export class Booking {
-    
+
     constructor(private id: number, private day: Date, private startingTime: string, private endingTime: string,
                 private sportsHallField: SportsHallField, private status: number, private user: User) {}
 
@@ -12,42 +12,42 @@ export class Booking {
     public set Id(v : number) {
         this.id = v;
     }
-    
+
     public get Day() : Date {
         return this.day;
     }
     public set Day(v : Date) {
         this.day = v;
     }
-    
+
     public get StartingTime() : string {
         return this.startingTime;
     }
     public set StartingTime(v : string) {
         this.startingTime = v;
     }
-    
+
     public get EndingTime() : string {
         return this.endingTime;
     }
     public set EndingTime(v : string) {
         this.endingTime = v;
     }
-    
+
     public get SportsHallField() : SportsHallField {
         return this.sportsHallField;
     }
     public set SportsHallField(v : SportsHallField) {
         this.sportsHallField = v;
     }
-    
+
     public get Status() : number {
         return this.status;
     }
     public set Status(v : number) {
         this.status = v;
     }
-    
+
     public get User() : User {
         return this.user;
     }
