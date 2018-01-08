30118ad503b9a7d1d022d70f206b795a48cf99fe
Bart in 't Veld <bart.int.007@gmail.com>
2018-01-08 15:01:59 +0100

    component added with test data

--- src/app/components/calendar/calendar.component.ts
+++ src/app/components/calendar/calendar.component.ts
@@ -1,4 +1,10 @@
+import { SportsHall } from './../../models/sportsHall.model';
+import { Field } from './../../models/field.model';
+import { SportsHallField } from './../../models/sportsHallField.model';
 import { Component, OnInit } from '@angular/core';
+import { Booking } from '../../models/booking.model';
+import { SportsFacility } from '../../models/sportsFacility.model';
+import { OpeningHours } from '../../models/openingHours.model';
 
 @Component({
   selector: 'app-calendar',
@@ -8,6 +14,9 @@ import { Component, OnInit } from '@angular/core';
 export class CalendarComponent implements OnInit {
 
   public hoursInADay: number[] = [];
+  public bookingsToDisplay = [];
+
+  private bookings = [{startingTime : '12:00', endingTime : '12:30', user : {UserName: 'bartaveld'}}, {startingTime : '16:00', endingTime : '17:30', user : {UserName: 'bartaveld'}}]
 
   constructor() {
     this.fillHours();
@@ -18,9 +27,15 @@ export class CalendarComponent implements OnInit {
 
   private fillHours(): void {
     var i: number;
-    for(i = 0; i < 24; i++ ) {
+    for(i = 6; i < 24; i++ ) {
       this.hoursInADay.push(i);
+      this.bookingsToDisplay.push([])
     }
-  }
 
+    this.bookings.forEach(booking => {
+      const firstNumber = parseInt(booking.startingTime.substring(0, 2));
+      console.log(firstNumber);
+      this.bookingsToDisplay[firstNumber].push(booking);
+    });
+  }
 }
