211dbf869755bb8ecef8b58184ba49971536f1a2
Niels van Dam <nielsvdam@hotmail.com>
2018-01-10 12:40:13 +0100

    dayoverview data

--- src/app/components/calendar/day/day.component.ts
+++ src/app/components/calendar/day/day.component.ts
@@ -1,6 +1,7 @@
 import { DateService } from './../../../services/date.service';
 import { Component, OnInit } from '@angular/core';
 import { OnChanges } from '@angular/core/src/metadata/lifecycle_hooks';
+import { BookingService } from '../../../services/booking.service';
 
 @Component({
   selector: 'app-day',
@@ -14,23 +15,27 @@ export class DayComponent implements OnInit {
   public highlightDatepicker: boolean = false;
   private highlightTimer;
 
-  constructor(private dateService: DateService) {
+  constructor(private dateService: DateService, private bookingService: BookingService) {
     this.date = new Date();
     this.dateString = this.dateService.fillInDateString(this.date)
   }
 
   ngOnInit() {
+    
+    this.bookingService.getBookingsByDate(new Date(this.dateString));
   }
 
   changeDate(date: string) {
     this.dateString = date;
     this.date = new Date(date);
+    this.bookingService.getBookingsByDate(new Date(this.dateString));
   }
 
   onDayChange(changeWith: number) {
     this.date.setDate(this.date.getDate() + changeWith);
     this.dateString = this.dateService.fillInDateString(this.date);
     this.highlightDayPicker();
+    this.bookingService.getBookingsByDate(new Date(this.dateString));
   }
 
   highlightDayPicker() {
--- src/app/services/booking.service.ts
+++ src/app/services/booking.service.ts
@@ -3,16 +3,18 @@ import { Http, Headers, URLSearchParams } from '@angular/http';
 import { environment } from '../../environments/environment';
 import {Booking} from '../models/booking.model';
 import {Subject} from 'rxjs/Subject';
+import { DateService } from './date.service';
 
 @Injectable()
 export class BookingService {
   private headers = new Headers({ 'Content-Type' : 'application/json'});
-  private serverUrl = environment.serverUrl + '/bookings';
+  private bookingsUrl = environment.serverUrl + '/bookings';
+  private dayOverviewUrl = environment.serverUrl + '/dayoverview';
 
   private bookings: Booking[];
   bookingsChanged = new Subject<Booking[]>();
 
-  constructor(private http: Http) {}
+  constructor(private http: Http, private dateService: DateService) {}
 
   private handleError(error: any): Promise<any> {
     console.log('handleError');
@@ -20,7 +22,7 @@ export class BookingService {
   }
 
   private getBookingsAsync() {
-    return this.http.get(this.serverUrl, {headers: this.headers})
+    return this.http.get(this.bookingsUrl, {headers: this.headers})
       .toPromise()
       .then((response) => {
         return response.json() as Booking;
@@ -38,6 +40,18 @@ export class BookingService {
       })
       .catch((rejected) => {
         console.log(rejected);
+      });
+  }
+  
+  getBookingsByDate(date: Date){
+    return this.http.get(this.dayOverviewUrl+'/1/1/'+this.dateService.fillInDateString(date,true), {headers: this.headers})
+      .toPromise()
+      .then((response) => {
+        console.log(response.json());
+        return response.json() as Booking[];
+      })
+      .catch((error) => {
+        return this.handleError(error);
       });
   }
 }
