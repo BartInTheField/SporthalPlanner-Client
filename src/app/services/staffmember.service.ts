902c4074d873558065248034f0a2b64c4d2a275c
Niels van Dam <nielsvdam@hotmail.com>
2018-01-15 13:01:38 +0100

    StaffMemberService added. Setup for functions added.

---
+++ src/app/services/staffmember.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers, URLSearchParams } from '@angular/http';
+import { environment } from '../../environments/environment';
+import {Subject} from 'rxjs/Subject';
+
+@Injectable()
+export class StaffMemberService {
+  private headers = new Headers({ 'Content-Type' : 'application/json'});
+  private staffMemberUrl = environment.serverUrl + '/staffmembers';
+
+
+  constructor(private http: Http) {}
+
+  private handleError(error: any): Promise<any> {
+    console.log('handleError');
+    return Promise.reject(error.message || error);
+  }
+
+  //NOTE: Deze functie is niet getest!!! 
+  public getBookings() {
+    return this.http.get(this.staffMemberUrl, {headers: this.headers})
+      .toPromise()
+      .then((response) => {
+        //this.staffmembers = response.json() as StaffMember[];
+        //this.staffMembersChanged.next(this.staffmembers.slice());
+      })
+      .catch((error) => {
+        return this.handleError(error);
+      });
+  }
+
+  //NOTE: Deze functie is niet getest!!! 
+  public getBooking() {
+    return this.http.get(this.staffMemberUrl, {headers: this.headers})
+      .toPromise()
+      .then((response) => {
+        //return response.json();
+      })
+      .catch((error) => {
+        return this.handleError(error);
+      });
+  }
+}
