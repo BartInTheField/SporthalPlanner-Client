417f38db90627e52e4034d798a67aac5f2cb18c0
Rick Voermans <rrw.voermans@student.avans.nl>
2018-01-16 12:42:25 +0100

    Add member

---
+++ src/app/components/work/add-member/add-member.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {StaffMember} from '../../../models/staffMember';
+import {StaffMemberService} from '../../../services/staffmember.service';
+
+@Component({
+  selector: 'app-add-member',
+  templateUrl: './add-member.component.html',
+  styleUrls: ['./add-member.component.scss']
+})
+export class AddMemberComponent implements OnInit {
+  private addmemberForm: FormGroup;
+  private staffmember: StaffMember;
+
+  constructor(private staffmemberService: StaffMemberService) {
+  }
+
+  ngOnInit() {
+    this.addmemberForm = new FormGroup({
+      'firstName': new FormControl('', Validators.required),
+      'lastName': new FormControl('', Validators.required),
+      'dateOfBirth': new FormControl('', Validators.required)
+    });
+  }
+
+  onAddStaffMember() {
+    this.onAddPromise()
+      .then(res => {
+        if(res === true) {
+          console.log('hij komt hier jehhhh')
+        }
+      });
+  }
+
+  onAddPromise() {
+    return new Promise((resolve, reject) => {
+      this.staffmember = this.addmemberForm.value;
+      this.staffmemberService.addStaffMember(this.staffmember);
+      this.addmemberForm = new FormGroup({
+        'firstName': new FormControl('', Validators.required),
+        'lastName': new FormControl('', Validators.required),
+        'dateOfBirth': new FormControl('', Validators.required)
+      });
+      resolve(true);
+    });
+  }
+}
